72be6c1b1aa16d4017546a0ccbb83818f30752ed
Benjamin <hoppeconnect@gmail.com>
2019-01-07 15:24:49 -0500

    completed assignment

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const results = document.querySelector('#electionResults');
 
-  // Imagination!
-
+  const request = axios({
+    methiod: 'GET',
+    url: 'https://bb-election-api.herokuapp.com/',
+    reponseType: 'text'
+  }).then((response) => {
+    response.data.candidates.forEach((candidate) => {
+      const candidateResult = document.createElement('p');
+      candidateResult.innerHTML = `Candidate Name: ${candidate.name}\n Votes: ${candidate.votes}`;
+      results.appendChild(candidateResult);
+        console.log(response.data);
+    });
+  });
 });
